67b4e7b43df74f24a0b79c2332902b08edd747e0
doctharock <doctharock@gmail.com>
2022-10-29 13:13:23 -0400

    JS day 3

---
+++ Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,44 @@
+// const country = "United States of America";
+// const continent = "North America";
+// let countryPop = 350000000;
+// let countryPopulation = countryPop.toLocaleString("en-US");
+// const isIsland = false;
+// const language = "English";
+// const description =
+//   "The " +
+//   country +
+//   " is in " +
+//   continent +
+//   ", and it's " +
+//   countryPopulation +
+//   " people speak " +
+//   language +
+//   ".";
+// const finland = "Finland";
+// let finlandPop = 6000000;
+// let finlandPopulation = finlandPop.toLocaleString("en-US");
+// let averagePop = 33000000;
+// let averagePopulation = averagePop.toLocaleString("en-US");
+
+// console.log(country, typeof country);
+// console.log(continent, typeof continent);
+// console.log(countryPopulation, typeof countryPopulation);
+// console.log(isIsland, typeof isIsland);
+// console.log(language, typeof language);
+
+// console.log(country + ", " + continent);
+
+// // Assignment Operators
+
+// // let x = 10 + 5; //15
+// // x += 10; // x = x + 10 = 25
+// // x *= 4; // x = x * 4 = 100
+// // x++; // x = x + 1 = 101
+// // x--; // x = x -1 = 100
+// // console.log(x);
+
+// console.log(country, countryPop / 2);
+// console.log(countryPop++);
+// console.log(countryPop > finlandPop);
+// console.log(countryPop > averagePop);
+// console.log(description);
--- Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/script.js
+++ Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,48 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log(40 + 8 + 23 - 10);
+// let firstName = "Jason",
+//   lastName = "Dockery";
+// console.log(firstName + " " + lastName);
+
+//const bmi = mass / height ** 2;
+// const markMass1 = 78;
+// const markHeight1 = 1.69;
+// const markBMI1 = markMass1 / markHeight1 ** 2;
+// const johnMass1 = 92;
+// const johnHeight1 = 1.95;
+// const johnBMI1 = johnMass1 / johnHeight1 ** 2;
+// const markHigherBMI1 = markBMI1 > johnBMI1;
+// console.log(
+//   "Mark's BMI is " +
+//     markBMI1 +
+//     ". John's BMI is " +
+//     johnBMI1 +
+//     ". Is Mark's BMI higher than John's? " +
+//     markHigherBMI1 +
+//     "."
+// );
+
+// const markMass2 = 95;
+// const markHeight2 = 1.88;
+// const markBMI2 = markMass2 / markHeight2 ** 2;
+// const johnMass2 = 85;
+// const johnHeight2 = 1.76;
+// const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+// const markHigherBMI2 = markBMI2 > johnBMI2;
+// console.log(
+//   "Mark's BMI is " +
+//     markBMI2 +
+//     ". John's BMI is " +
+//     johnBMI2 +
+//     ". Is Mark's BMI higher than John's? " +
+//     markHigherBMI2 +
+//     "."
+// );
+
+const firstName = "Jason";
+const job = "Senior Analyst";
+const birthYear = "1979";
+let year = 2022;
+const jason =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job;
