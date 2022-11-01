13e72b0bf068ef16d7ce2109b375bcdf6966e918
doctharock <doctharock@gmail.com>
2022-11-01 17:12:32 -0400

    section 2 chapter 26

--- Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/assignments.js
+++ Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,44 +1,116 @@
-// const country = "United States of America";
-// const continent = "North America";
-// let countryPop = 350000000;
-// let countryPopulation = countryPop.toLocaleString("en-US");
-// const isIsland = false;
-// const language = "English";
-// const description =
-//   "The " +
-//   country +
-//   " is in " +
-//   continent +
-//   ", and it's " +
-//   countryPopulation +
-//   " people speak " +
-//   language +
-//   ".";
-// const finland = "Finland";
-// let finlandPop = 6000000;
-// let finlandPopulation = finlandPop.toLocaleString("en-US");
-// let averagePop = 33000000;
-// let averagePopulation = averagePop.toLocaleString("en-US");
+const country = "The United States of America";
+const continent = "North America";
+let countryPop = 350000000;
+let countryPopulation = countryPop.toLocaleString("en-US");
+const isIsland = false;
+const language = `Spanish`;
+console.log(language);
+const averagePop = 33000000;
+const averagePopulation = averagePop.toLocaleString("en-US");
+const description = `"The ${country} is in ${continent}, and it's ${countryPopulation} people speak ${language}.`;
 
-// console.log(country, typeof country);
-// console.log(continent, typeof continent);
-// console.log(countryPopulation, typeof countryPopulation);
-// console.log(isIsland, typeof isIsland);
-// console.log(language, typeof language);
+// if (countryPop >= 33000000) {
+//   console.log(
+//     `The United States of America's Population is ${
+//       countryPop - averagePop
+//     } above average.`
+//   );
+// } else {
+//   console.log(
+//     `The United States of America's population is ${
+//       averagePop - countryPop
+//     } below average.`
+//   );
+// }
 
-// console.log(country + ", " + continent);
+const finland = "Finland";
+let finlandPop = 6000000;
+let finlandPopulation = finlandPop.toLocaleString("en-US");
 
-// // Assignment Operators
+switch (language) {
+  case `Mandarin` || `Chinese`:
+    console.log(`MOST number of native speakers!`);
+    break;
+  case `Spanish`:
+    console.log(`2nd most native speakers.`);
+    break;
+  case `English`:
+    console.log(`3rd most native speakers.`);
+    break;
+  case `Hindi`:
+    console.log(`4th most native speakers.`);
+    break;
+  case `Arabic`:
+    console.log(`5th most native speakers.`);
+    break;
+  default:
+    console.log(`Great language as well!`);
+}
 
-// // let x = 10 + 5; //15
-// // x += 10; // x = x + 10 = 25
-// // x *= 4; // x = x * 4 = 100
-// // x++; // x = x + 1 = 101
-// // x--; // x = x -1 = 100
-// // console.log(x);
+// if (language === "English" && countryPop > 50000000 && !isIsland) {
+//   console.log(`You should live in ${country}!`);
+// } else {
+//   console.log(`${country} does not meet your criteria.`);
+// }
 
-// console.log(country, countryPop / 2);
-// console.log(countryPop++);
-// console.log(countryPop > finlandPop);
-// console.log(countryPop > averagePop);
-// console.log(description);
+// // console.log(country, typeof country);
+// // console.log(continent, typeof continent);
+// // console.log(countryPopulation, typeof countryPopulation);
+// // console.log(isIsland, typeof isIsland);
+// // console.log(language, typeof language);
+
+// // console.log(country + ", " + continent);
+
+// // // Assignment Operators
+
+// // // let x = 10 + 5; //15
+// // // x += 10; // x = x + 10 = 25
+// // // x *= 4; // x = x * 4 = 100
+// // // x++; // x = x + 1 = 101
+// // // x--; // x = x -1 = 100
+// // // console.log(x);
+
+// // console.log(country, countryPop / 2);
+// // console.log(countryPop++);
+// // console.log(countryPop > finlandPop);
+// // console.log(countryPop > averagePop);
+// // console.log(description);
+
+// // const numNeighbors = Number(
+// //   prompt(`How many neighbor countries does your country have?`)
+// // );
+// // if (numNeighbors === 1) {
+// //   console.log(`Only one border.`);
+// // } else if (numNeighbors > 1) {
+// //   console.log(`More than 1 border`);
+// // } else if (numNeighbors === 0) {
+// //   console.log(`There are no borders`);
+// // }
+
+// // // const money = 0;
+// // // if (money) {
+// // //   console.log(`Don't spend it all 🤪`);
+// // // } else {
+// // //   console.log(`Get to work you bum!`);
+// // // }
+
+// // // let height;
+// // // if (height) {
+// // //   console.log(`Height is defined`);
+// // // } else {
+// // //   console.log(`Height is undefined`);
+// // // }
+
+// // // const favorite = Number(prompt(`What's your favorite number?`));
+
+// // // if (favorite === 23) {
+// // //   console.log(`Nice! 23 is the goat!`);
+// // // } else if (favorite === 7) {
+// // //   console.log(`7 is aight.`);
+// // // } else if (favorite === 5) {
+// // //   console.log(`5 is the best number!`);
+// // // } else {
+// // //   console.log(`That's a bad number.`);
+// // // }
+
+// // // if (favorite !== 5) console.log(`Why not 5?`);
--- Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/script.js
+++ Web Development/jsCourseMaster/01-Fundamentals-Part-1/starter/script.js
@@ -5,7 +5,6 @@
 //   lastName = "Dockery";
 // console.log(firstName + " " + lastName);
 
-//const bmi = mass / height ** 2;
 // const markMass1 = 78;
 // const markHeight1 = 1.69;
 // const markBMI1 = markMass1 / markHeight1 ** 2;
@@ -13,6 +12,7 @@
 // const johnHeight1 = 1.95;
 // const johnBMI1 = johnMass1 / johnHeight1 ** 2;
 // const markHigherBMI1 = markBMI1 > johnBMI1;
+
 // console.log(
 //   "Mark's BMI is " +
 //     markBMI1 +
@@ -40,9 +40,161 @@
 //     "."
 // );
 
-const firstName = "Jason";
-const job = "Senior Analyst";
-const birthYear = "1979";
-let year = 2022;
-const jason =
-  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job;
+// if (markBMI1 > johnBMI1) {
+//   console.log(
+//     `Mark's BMI of ${markBMI1} is higher than John's BMI of ${johnBMI1}`
+//   );
+// } else {
+//   console.log(
+//     `John's BMI of ${johnBMI1} is higher than Mark's BMI of ${markBMI1}`
+//   );
+// }
+
+// if (markBMI2 > johnBMI2) {
+//   console.log(
+//     `Mark's BMI of ${markBMI2} is higher than John's BMI of ${johnBMI2}`
+//   );
+// } else {
+//   console.log(
+//     `John's BMI of ${johnBMI2} is higher than Mark's BMI of ${markBMI2}`
+//   );
+// }
+
+// const firstName = "Jason";
+// const job = "Senior Analyst";
+// const birthYear = "1979";
+// let year = 2022;
+// const jason =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+// console.log(jason);
+
+// const jasonNew = `I'm ${firstName} a ${year - birthYear} year old ${job}!`;
+
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// // if (shouldDrive) {
+// //   console.log(`Sarah is able to drive.`);
+// // } else {
+// //   console.log(`Someone else should drive.`);
+// // }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log(`Sarah is able to drive!`);
+// } else {
+//   console.log(`Someone else should drive...`);
+// }
+
+// const teamOneName = "Dragons";
+// const teamTwoName = "Koalas";
+
+// const teamOneScore = (96 + 108 + 89) / 3;
+// const teamTwoScore = (96 + 108 + 89) / 3; // original scores 88 + 91 + 110
+
+// if (teamOneScore > teamTwoScore) {
+//   console.log(
+//     `The ${teamOneName} wins the game with an average score of ${teamOneScore}!`
+//   );
+// } else if (teamOneScore === teamTwoScore) {
+//   console.log(`It's a tie with an average score of ${teamOneScore}!`);
+// } else if (teamTwoScore > teamOneScore) {
+//   console.log(
+//     `The ${teamTwoName} wins the game with an average score of ${teamTwoScore}!`
+//   );
+// }
+
+// Bonus 1
+
+// const teamOneName = "Dragons";
+// const teamTwoName = "Koalas";
+
+// const teamOneScore = (97 + 112 + 101) / 3;
+// const teamTwoScore = (109 + 95 + 123) / 3; // original scores 88 + 91 + 110
+
+// if (teamOneScore > teamTwoScore && teamOneScore >= 100) {
+//   console.log(
+//     `The ${teamOneName} wins the game with an average score of ${teamOneScore}!`
+//   );
+// } else if (teamOneScore === teamTwoScore) {
+//   console.log(`It's a tie with an average score of ${teamOneScore}!`);
+// } else if (teamTwoScore > teamOneScore && teamTwoScore >= 100) {
+//   console.log(
+//     `The ${teamTwoName} wins the game with an average score of ${teamTwoScore}!`
+//   );
+// }
+
+// Bonus 2
+
+// const teamOneName = "Dragons";
+// const teamTwoName = "Koalas";
+
+// const teamOneScore = (97 + 112 + 85) / 3;
+
+// const teamTwoScore = (109 + 95 + 86) / 3; // original scores 88 + 91 + 110
+// console.log(teamOneScore, teamTwoScore);
+
+// if (teamOneScore > teamTwoScore && teamOneScore >= 100) {
+//   console.log(
+//     `The ${teamOneName} wins the game with an average score of ${teamOneScore}!`
+//   );
+// } else if (
+//   teamOneScore === teamTwoScore &&
+//   teamOneScore >= 100 &&
+//   teamTwoScore
+// ) {
+//   console.log(`It's a tie with an average score of ${teamOneScore}!`);
+// } else if (teamTwoScore > teamOneScore && teamTwoScore >= 100) {
+//   console.log(
+//     `The ${teamTwoName} wins the game with an average score of ${teamTwoScore}!`
+//   );
+// } else {
+//   console.log(`Nobody wins. Losers!`);
+// }
+
+// const day = `sunday`;
+
+// switch (day) {
+//   case `monday`:
+//     console.log(`Plan course structure.`);
+//     console.log(`Go to coding meetup.`);
+//     break;
+//   case `tuesday`:
+//     console.log(`Prepare theory videos.`);
+//     break;
+//   case `wednesday`:
+//   case `thursday`:
+//     console.log(`Write code examples.`);
+//     break;
+//   case `friday`:
+//     console.log(`Record videos`);
+//     break;
+//   case `saturday`:
+//   case `sunday`:
+//     console.log(`Enjoy the weekend!`);
+//     break;
+//   default:
+//     console.log(`Not a valid day!`);
+// }
+
+// if (day === `monday`) {
+//   console.log(`Plan course Structure.`, `Go to coding meetup.`);
+// } else if (day === `tuesday`) {
+//   console.log(`Prepare theory videos.`);
+// } else if (day === `wednesday` || day === `thursday`) {
+//   console.log(`Write Code Examples.`);
+// } else if (day === `friday`) {
+//   console.log(`Record videos`);
+// } else if (day === `saturday` || day === `sunday`) {
+//   console.log(`Enjoy the weekend!`);
+// } else {
+//   console.log(`Not a valid day!`);
+// }
